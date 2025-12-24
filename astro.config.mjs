9f1f0096fa3a6c135c49ef7b3e138695e16416ea
julio santacruz <julio.santacruzh@gmail.com>
2025-12-24 15:39:24 -0800

    astro confir

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juliosantacruz.dev',
+  base: '/metodo-kc12/',
   vite: {
     plugins: [tailwindcss()]
   }
